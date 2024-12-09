d8a95321423d13f91c896c851e9ebbb5a9dbdeb0
listen <listen2code@gmail.com>
2024-12-09 09:19:21 +0900

    add: logger

---
+++ lib/base/utils/logger_util.dart
@@ -0,0 +1,76 @@
+import 'package:get/get.dart';
+import 'package:logger/logger.dart';
+
+class LoggerUtil {
+  static final Logger logger = Logger(
+      filter: null,
+      printer: PrettyPrinter(
+        stackTraceBeginIndex: 2,
+        methodCount: 2,
+        colors: true,
+        printEmojis: false,
+        printTime: true,
+        noBoxingByDefault: false,
+      ),
+      output: null);
+
+  static void log(dynamic message, {LoggerType type = LoggerType.info}) {
+    Logger.level = Level.all;
+    loggerWithType(message, type: type);
+  }
+
+  static void loggerWithType(
+    dynamic message, {
+    LoggerType type = LoggerType.debug,
+  }) {
+    switch (type) {
+      case LoggerType.trace:
+        logger.t(message);
+        break;
+      case LoggerType.debug:
+        logger.d(message);
+        break;
+      case LoggerType.info:
+        logger.i(message);
+        break;
+      case LoggerType.warning:
+        logger.w(message);
+        break;
+      case LoggerType.error:
+        logger.e(message);
+        break;
+      case LoggerType.fatal:
+        logger.f(message);
+        break;
+      case LoggerType.easy:
+        logger.printInfo(info: message.toString());
+        break;
+      default:
+        logger.d(message);
+        break;
+    }
+  }
+
+  static void warning(dynamic message,
+      {Object? error, StackTrace? stackTrace}) {
+    logger.w(message, error: error, stackTrace: stackTrace);
+  }
+
+  static void error(dynamic message, {Object? error, StackTrace? stackTrace}) {
+    logger.e(message, error: error, stackTrace: stackTrace);
+  }
+}
+
+enum LoggerType {
+  trace,
+  debug,
+  info,
+  warning,
+  error,
+  fatal,
+  easy,
+}
+
+void log(dynamic message, {LoggerType type = LoggerType.easy}) {
+  LoggerUtil.loggerWithType(message, type: type);
+}
