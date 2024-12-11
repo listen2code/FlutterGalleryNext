756c70261da9bece13aa7b042f66707bf303d20d
listen <listen2code@gmail.com>
2024-12-11 09:58:21 +0900

    add: connectivity_util

---
+++ library/package_libs/lib/utils/connectivity_util.dart
@@ -0,0 +1,23 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+class ConnectivityUtil {
+  ConnectivityUtil._private();
+
+  static Future<String> getStatus() async {
+    List<ConnectivityResult> connectivityResult =
+        await (Connectivity().checkConnectivity());
+
+    switch (connectivityResult.first) {
+      case ConnectivityResult.wifi:
+        return Future.value("WiFi");
+      case ConnectivityResult.mobile:
+        return Future.value("Mobile");
+      case ConnectivityResult.none:
+      case ConnectivityResult.bluetooth:
+      case ConnectivityResult.ethernet:
+      case ConnectivityResult.vpn:
+      case ConnectivityResult.other:
+        return Future.value("UNKNOWN");
+    }
+  }
+}
