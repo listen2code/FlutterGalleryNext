1afdebaeb3b9ca9c11d31dfb1d21106bfc3067ec
listen2code@gmail.com <listen2code@gmail.com>
2025-11-23 19:40:08 +0900

    add: AutoLoadWidget

---
+++ lib/base/widget/base_auto_load_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_gallery_next/base/mvvm/view_mode/view_mode.dart';
+import 'package:flutter_gallery_next/base/network/base/base_api_use_case.dart';
+import 'package:get/get.dart';
+
+class AutoLoadWidget<T, VM extends ViewMode> extends StatelessWidget {
+  final VM viewMode;
+  final Rx<ResponseEntity<T>> rxResponse;
+  final Widget Function(T data) widget;
+  final Widget Function(String? error)? onError;
+  final Widget Function(String? error)? onSystemError;
+  final Widget Function(String? error)? onSessionTimeout;
+  final Widget Function(String? error)? onNetworkError;
+  final Widget? onLoading;
+
+  final Widget? onEmpty;
+
+  final bool autoEmpty;
+
+  const AutoLoadWidget({
+    super.key,
+    required this.viewMode,
+    required this.rxResponse,
+    required this.widget,
+    this.onError,
+    this.onSystemError,
+    this.onSessionTimeout,
+    this.onNetworkError,
+    this.onLoading,
+    this.onEmpty,
+    this.autoEmpty = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => rxResponse.value.observe(
+        (data) => widget(data),
+        onError: onError,
+        onSystemError: onSystemError,
+        onSessionTimeout: onSessionTimeout,
+        onNetworkError: onNetworkError,
+        onLoading: onLoading,
+        onEmpty: onEmpty,
+        autoEmpty: autoEmpty,
+      ),
+    );
+  }
+}
