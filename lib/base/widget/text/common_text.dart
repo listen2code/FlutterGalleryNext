912977e4118535573278a91be8abaa318988c150
listen2code@gmail.com <listen2code@gmail.com>
2026-02-07 10:42:54 +0900

    refactor: lib/base/widget/text/common_text.dart (update CommonText props and build logic)

--- lib/base/widget/text/common_text.dart
+++ lib/base/widget/text/common_text.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class CommonText extends Text {
   const CommonText(
-    super.data, {
+    super.text, {
     super.key,
     super.style,
     super.strutStyle,
@@ -17,34 +17,46 @@ class CommonText extends Text {
     super.textWidthBasis,
     super.textHeightBehavior,
     super.selectionColor,
-    this.commonFit,
-    this.alignment,
+    this.fit = BoxFit.scaleDown,
+    this.alignment = Alignment.centerLeft,
+    this.containerOptions,
+    this.useFittedBox = true,
   });
 
-  final BoxFit? commonFit;
-  final AlignmentGeometry? alignment;
+  final BoxFit fit;
+
+  final AlignmentGeometry alignment;
+
+  final ContainerOptions? containerOptions;
+
+  final bool useFittedBox;
 
   @override
   Widget build(BuildContext context) {
-    return _commonText(context);
-  }
+    Widget child = super.build(context);
 
-  Widget _commonText(BuildContext context) => FittedBox(
-        fit: commonFit ?? BoxFit.contain,
-        alignment: alignment ?? Alignment.center,
-        child: super.build(context),
+    if (useFittedBox) {
+      child = FittedBox(fit: fit ?? BoxFit.contain, alignment: alignment ?? Alignment.center, child: child);
+    }
+
+    if (containerOptions != null) {
+      return Container(
+        height: containerOptions!.height,
+        padding: containerOptions!.padding,
+        margin: containerOptions!.margin,
+        alignment: containerOptions!.alignment,
+        decoration: containerOptions!.decoration ??
+            (containerOptions!.backgroundColor != null ? BoxDecoration(color: containerOptions!.backgroundColor) : null),
+        child: child,
       );
+    }
+
+    return child;
+  }
 }
 
 class ContainerOptions {
-  ContainerOptions({
-    this.height,
-    this.padding,
-    this.margin,
-    this.backgroundColor,
-    this.decoration,
-    this.alignment,
-  });
+  const ContainerOptions({this.height, this.padding, this.margin, this.backgroundColor, this.decoration, this.alignment});
 
   final double? height;
   final EdgeInsetsGeometry? padding;
